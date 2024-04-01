964a52535b7b2d6b4c4c43f172eb026457a3329b
Matheus <matheus.mhg2@gmail.com>
2024-04-01 10:17:03 -0300

    feat: antdesign in navbar components

--- src/components/FeaturedProperties/FeaturedProperties.jsx
+++ src/components/FeaturedProperties/FeaturedProperties.jsx
@@ -3,7 +3,7 @@ import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
   const { data, loading } = useFetch(
-    "http://localhost:4000/hotels?featured=true&limit=4"
+    "http://localhost:4000/hotels/featured?featured=true&limit=4"
   );
 
   return (
@@ -14,11 +14,7 @@ const FeaturedProperties = () => {
         <>
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
-              <img
-                src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-                alt=""
-                className="fpImg"
-              />
+              <img src={item.photos[0]} alt="" className="fpImg" />
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">
