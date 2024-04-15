fc3899b2fcbda36468f4f116150530a33367104b
matheushenriquecsb <matheus.mhg2@gmail.com>
2024-04-15 18:00:53 -0300

    feat: create oauth github and google component

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,18 +1,16 @@
-import axios from "axios";
 import { InfoCircleOutlined, LoadingOutlined } from "@ant-design/icons";
 import { Input, Tooltip } from "antd";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import Navbar from "../../components/Navbar/Navbar";
 import "./register.css";
 
-export default function Signup() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
-
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -31,35 +29,35 @@ export default function Signup() {
       navigate("/signin");
     } catch (error) {
       setLoading(false);
-      setError(true);
+      throw new Error(error);
     }
   };
 
   return (
     <div>
       <Navbar />
-      <div className="signupContainer">
-        <h1 className="signupTitle">crie uma conta</h1>
+      <div className="formContainer">
+        <h1 className="formTitle">crie sua conta</h1>
         <form onSubmit={handleSubmit} className="inputContainer">
-          <h4>Nome de Usuário</h4>
+          <h4>Nome</h4>
           <Input
-            placeholder="Insira um nome de usúario"
+            placeholder="Insira seu nome"
             type="text"
-            id="username"
+            id="fullName"
             className="inputUsername"
-            onChange={handleChange}
             suffix={
-              <Tooltip title="No máximo 8 caracteres">
+              <Tooltip title="No máximo 50 caracteres">
                 <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
               </Tooltip>
             }
+            onChange={handleChange}
           />
           <h4>Email</h4>
           <Input
             size="large"
             type="text"
             id="email"
-            placeholder="Insira seu Email"
+            placeholder="Insira seu email"
             className="inputEmail"
             onChange={handleChange}
           />
@@ -67,27 +65,24 @@ export default function Signup() {
           <Input.Password
             type="text"
             id="password"
-            placeholder="Senha"
+            placeholder="Insira sua senha"
             className="inputPassword"
+            onChange={handleChange}
             visibilityToggle={{
               visible: passwordVisible,
               onVisibleChange: setPasswordVisible,
             }}
-            onChange={handleChange}
           />
-          <button className="signupButton" disabled={loading}>
+          <button className="formButton">
             {loading ? <LoadingOutlined /> : "Cadastre-se"}
           </button>
         </form>
-        <div className="loginContainer">
+        <div className="formText">
           <p>Já possui conta?</p>
-          <Link to="/signin">
-            <span className="loginText">Login</span>
+          <Link className="formLink" to="/signin">
+            <span>Login</span>
           </Link>
         </div>
-        <p className={`erroContainer ${error ? "errorSpan" : "okSpan"}`}>
-          {error ? "Erro ao cadastrar" : ""}
-        </p>
       </div>
     </div>
   );
